a410ac52d9d4deeae2b0a38ae6d13ba248dbc00d
WilliamLiu <s359119@yo-woo.com>
2019-05-20 13:48:22 +0800

    add calender date cell

---
+++ Calender/CalenderViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Calender
+//
+//  Created by 劉士維 on 2019/5/20.
+//  Copyright © 2019 LPB. All rights reserved.
+//
+
+import UIKit
+
+class CalenderViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
+extension CalenderViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 7
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CalenderDateCell", for: indexPath) as! CalenderDateCell
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 70, height: 90)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    
+}
